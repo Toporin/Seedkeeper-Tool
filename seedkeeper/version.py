37dbeadab4409b951ccc5ace2212769428fac26c
Toporin <satochip.wallet@gmail.com>
2024-03-28 14:20:20 +0000

    SeedKeeperTool v0.1.3

--- seedkeeper/version.py
+++ seedkeeper/version.py
@@ -2,4 +2,5 @@
 # v0.1.0: initial release
 # v0.1.1: MacOS support (beta)
 # v0.1.2: SeedQR support + fix minor bugs
-SEEDKEEPERTOOL_VERSION = '0.1.2' # version of the client package
+# v0.1.3: Patch https://github.com/Toporin/Seedkeeper-Tool/issues/12
+SEEDKEEPERTOOL_VERSION = '0.1.3' # version of the client package
